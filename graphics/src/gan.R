568b87cf56515e6e2ad4fff5e15ae64d55649b10
N. Greffard <nicolas.greffard@expandium.com>
2018-03-08 14:26:20 +0100

    feat: 3 channel dcgan

--- graphics/src/gan.R
+++ graphics/src/gan.R
@@ -1,112 +1,172 @@
 library(keras)
 library(progress)
 library(abind)
+library(grid)
+library(magick)
 k_set_image_data_format('channels_first')
 
 # Functions ---------------------------------------------------------------
 
-build_generator <- function(latent_size){
+
+plotImages <- function(image){
+  n <- floor(sqrt(dim(image)[1]))
+  if(length(dim(image))>3){
+    channel_first <- FALSE
+    x <- dim(image)[2]
+    y <- dim(image)[3]
+    z <- dim(image)[4]
+    if(dim(image)[4]>dim(image)[2]){
+      channel_first <- TRUE
+      x <- dim(image)[3]
+      y <- dim(image)[4]
+      z <- dim(image)[2]
+    } 
+    index <- 1
+    imgs <- c()
+    for(i in 1:n){
+      img <- c()
+      for(j in 1:n){
+        c_img <- image[index,,,]
+        dim(c_img) <- c(x,y,z)
+        img <- abind(img, c_img, along = 1)
+        index <- index + 1
+      }
+      imgs <- abind(imgs, img, along = 2)
+    }
+    image <- imgs
+    if(z == 1){
+      image = abind(image, imgs, along = 3)
+      image = abind(image, imgs, along = 3)
+    }
+  }
+  image = (image + 1)/2
+  image[image<=0] = .Machine$double.eps
+  image[image>=1] = 1- .Machine$double.eps
+  grid.raster(EBImage::rotate(image, 180))
+}
+
+
+build_generator <- function(latent_size, channels = 1){
   
-  # We will map a pair of (z, L), where z is a latent vector and L is a
-  # label drawn from P_c, to image space (..., 1, 28, 28)
   cnn <- keras_model_sequential()
   
   cnn %>%
-    layer_dense(1024, input_shape = latent_size, activation = "relu") %>%
-    layer_dense(128*7*7, activation = "relu") %>%
+    layer_dense(1024, input_shape = latent_size, activation = "tanh") %>%
+    layer_dense(128*7*7, activation = "tanh") %>%
+    layer_batch_normalization() %>%
     layer_reshape(c(128, 7, 7)) %>%
     # Upsample to (..., 14, 14)
     layer_upsampling_2d(size = c(2, 2)) %>%
     layer_conv_2d(
-      256, c(5,5), padding = "same", activation = "relu",
+      64, c(5,5), padding = "same", activation = "tanh",
       kernel_initializer = "glorot_normal"
     ) %>%
     # Upsample to (..., 28, 28)
     layer_upsampling_2d(size = c(2, 2)) %>%
-    layer_conv_2d(
-      128, c(5,5), padding = "same", activation = "tanh",
-      kernel_initializer = "glorot_normal"
-    ) %>%
+    # layer_conv_2d(
+    #   128, c(5,5), padding = "same", activation = "tanh",
+    #   kernel_initializer = "glorot_normal"
+    # ) %>%
     # Take a channel axis reduction
     layer_conv_2d(
-      1, c(2,2), padding = "same", activation = "tanh",
+      channels, c(5,5), padding = "same", activation = "tanh",
       kernel_initializer = "glorot_normal"
     )
   
-  
-  # This is the z space commonly referred to in GAN papers
   latent <- layer_input(shape = list(latent_size))
+  fake_image <- cnn(latent)
   
-  # This will be our label
-  image_class <- layer_input(shape = list(1))
-  
-  # 10 classes in MNIST
-  cls <-  image_class %>%
-    layer_embedding(
-      input_dim = 10, output_dim = latent_size, 
-      embeddings_initializer='glorot_normal'
-    ) %>%
-    layer_flatten()
-  
-  
-  # Hadamard product between z-space and a class conditional embedding
-  h <- layer_multiply(list(latent, cls))
-  
-  fake_image <- cnn(h)
-  
-  keras_model(list(latent, image_class), fake_image)
+  keras_model(latent, fake_image)
 }
 
-build_discriminator <- function(){
+build_discriminator <- function(channels = 1){
   
-  # Build a relatively standard conv net, with LeakyReLUs as suggested in
-  # the reference paper
   cnn <- keras_model_sequential()
   
   cnn %>%
     layer_conv_2d(
-      32, c(3,3), padding = "same", strides = c(2,2),
-      input_shape = c(1, 28, 28)
+      64, c(5,5), 
+      padding = "same", 
+      strides = c(2,2),
+      input_shape = c(channels, 28, 28), activation = "tanh"
     ) %>%
-    layer_activation_leaky_relu() %>%
-    layer_dropout(0.3) %>%
+    #layer_activation_leaky_relu() %>%
+    #layer_dropout(0.3) %>%
+    layer_max_pooling_2d(pool_size = c(2,2)) %>%
+    layer_conv_2d(
+      128, c(5, 5), 
+      padding = "same", 
+      strides = c(1,1),
+      activation = "tanh") %>%
+    #layer_activation_leaky_relu() %>%
+    #layer_dropout(0.3) %>%  
+    layer_max_pooling_2d(pool_size = c(2,2)) %>%
     
-    layer_conv_2d(64, c(3, 3), padding = "same", strides = c(1,1)) %>%
-    layer_activation_leaky_relu() %>%
-    layer_dropout(0.3) %>%  
-    
-    layer_conv_2d(128, c(3, 3), padding = "same", strides = c(2,2)) %>%
-    layer_activation_leaky_relu() %>%
-    layer_dropout(0.3) %>%  
-    
-    layer_conv_2d(256, c(3, 3), padding = "same", strides = c(1,1)) %>%
-    layer_activation_leaky_relu() %>%
-    layer_dropout(0.3) %>%  
+    # layer_conv_2d(128, c(3, 3), padding = "same", strides = c(2,2)) %>%
+    # layer_activation_leaky_relu() %>%
+    # layer_dropout(0.3) %>%  
+    # 
+    # layer_conv_2d(256, c(3, 3), padding = "same", strides = c(1,1)) %>%
+    # layer_activation_leaky_relu() %>%
+    # layer_dropout(0.3) %>%  
     
     layer_flatten()
   
   
   
-  image <- layer_input(shape = c(1, 28, 28))
+  image <- layer_input(shape = c(channels, 28, 28))
   features <- cnn(image)
   
-  # First output (name=generation) is whether or not the discriminator
-  # thinks the image that is being shown is fake, and the second output
-  # (name=auxiliary) is the class that the discriminator thinks the image
-  # belongs to.
   fake <- features %>% 
+    # layer_dense(128, activation = "tanh") %>%
+    # layer_dense(64, activation = "tanh") %>%
+    # layer_dense(32, activation = "tanh") %>%
+    #test with only one dense layer
+    layer_dense(1024, activation = "tanh") %>% 
     layer_dense(1, activation = "sigmoid", name = "generation")
   
-  aux <- features %>%
-    layer_dense(10, activation = "softmax", name = "auxiliary")
-  
-  keras_model(image, list(fake, aux))
+  keras_model(image, fake)
 }
+
+
+# Data Preparation --------------------------------------------------------
+
+d <- readRDS("../data/proc/block//cdata.dat")
+#dd <- readRDS("../data/proc/pg/data.dat")
+#d <- abind(d,dd,along = 1)
+d = abind(d,d, along = 1)
+
+channels = 1
+if(length(dim(d))>3){
+  channels = dim(d)[4]
+}
+
+imgs = NULL
+index = 1
+plotImages(d)
+
+train_indexes <- sample(1:dim(d)[1],size = floor(0.8*dim(d)[1]))
+test_indexes <- (1:dim(d)[1])[!(1:dim(d)[1])%in%train_indexes]
+dataset = c()
+if(channels>1){
+  dataset$train$x <- d[train_indexes,,,]
+  dataset$test$x <- d[test_indexes,,,]
+}else{
+  dataset$train$x <- d[train_indexes,,]
+  dataset$test$x <- d[test_indexes,,]  
+}
+
+
+dataset$train$x <- array_reshape(dataset$train$x, c(length(train_indexes), channels, 28, 28))
+dataset$test$x <- array_reshape(dataset$test$x, c(length(test_indexes), channels, 28, 28))
+
+num_train <- length(train_indexes)
+num_test <- length(test_indexes)
 
 # Parameters --------------------------------------------------------------
 
 # Batch and latent size taken from the paper
-epochs <- 50
+epochs <- 200 #on ghosts only; 200 too much; 100 good threshold
 batch_size <- 5
 latent_size <- 100
 
@@ -117,56 +177,36 @@ adam_beta_1 <- 0.5
 # Model Definition --------------------------------------------------------
 
 # Build the discriminator
-discriminator <- build_discriminator()
+discriminator <- build_discriminator(channels)
 discriminator %>% compile(
-  optimizer = optimizer_adam(lr = adam_lr, beta_1 = adam_beta_1),
-  loss = list("binary_crossentropy", "sparse_categorical_crossentropy")
+  #optimizer = optimizer_adam(lr = adam_lr, beta_1 = adam_beta_1),
+  optimize = optimizer_sgd(lr = 0.0005, momentum = 0.9, nesterov = TRUE),
+  loss = "binary_crossentropy"
 )
 
 # Build the generator
-generator <- build_generator(latent_size)
+
+generator <- build_generator(latent_size,channels)
 generator %>% compile(
-  optimizer = optimizer_adam(lr = adam_lr, beta_1 = adam_beta_1),
+  #optimizer = optimizer_adam(lr = adam_lr, beta_1 = adam_beta_1),
+  optimize = "SGD",
   loss = "binary_crossentropy"
 )
 
 latent <- layer_input(shape = list(latent_size))
-image_class <- layer_input(shape = list(1), dtype = "int32")
-
-fake <- generator(list(latent, image_class))
+fake <- generator(latent)
 
 # Only want to be able to train generation for the combined model
 freeze_weights(discriminator)
 results <- discriminator(fake)
 
-combined <- keras_model(list(latent, image_class), results)
+combined <- keras_model(latent, results)
 combined %>% compile(
-  optimizer = optimizer_adam(lr = adam_lr, beta_1 = adam_beta_1),
-  loss = list("binary_crossentropy", "sparse_categorical_crossentropy")
+  #optimizer = optimizer_adam(lr = adam_lr, beta_1 = adam_beta_1),
+  optimize = optimizer_sgd(lr = 0.0005, momentum = 0.9, nesterov = TRUE),
+  loss = "binary_crossentropy"
 )
 
-# Data Preparation --------------------------------------------------------
-
-# Loade mnist data, and force it to be of shape (..., 1, 28, 28) with
-# range [-1, 1]
-d <- readRDS("../data/proc/gm/data.dat")
-dd <- readRDS("../data/proc/pg/data.dat")
-d <- abind(d,dd,along = 1)
-labels = array(0, dim(d)[1])
-labels[1:dim(d)[1]] = 1
-train_indexes <- sample(1:dim(d)[1],size = floor(0.8*dim(d)[1]))
-test_indexes <- (1:dim(d)[1])[!(1:dim(d)[1])%in%train_indexes]
-dataset = c()
-dataset$train$x <- d[train_indexes,,]
-dataset$train$y <- labels[train_indexes]
-dataset$test$x <- d[test_indexes,,]
-dataset$test$y <- labels[test_indexes]
-
-dataset$train$x <- array_reshape(dataset$train$x, c(length(train_indexes), 1, 28, 28))
-dataset$test$x <- array_reshape(dataset$test$x, c(length(test_indexes), 1, 28, 28))
-
-num_train <- length(train_indexes)
-num_test <- length(test_indexes)
 
 # Training ----------------------------------------------------------------
 
@@ -189,33 +229,27 @@ for(epoch in 1:epochs){
     pb$tick()
     
     # Generate a new batch of noise
-    noise <- runif(n = batch_size*latent_size, min = 0, max = 1) %>%
+    noise <- runif(n = batch_size*latent_size, min = -1, max = 1) %>%
       matrix(nrow = batch_size, ncol = latent_size)
     
     # Get a batch of real images
     batch <- sample(possible_indexes, size = batch_size)
     possible_indexes <- possible_indexes[!possible_indexes %in% batch]
     image_batch <- dataset$train$x[batch,,,,drop = FALSE]
-    label_batch <- dataset$train$y[batch]
-    
-    # Sample some labels from p_c
-    sampled_labels <- sample(0:1, batch_size, replace = TRUE) %>%
-      matrix(ncol = 1)
-    
+
     # Generate a batch of fake images, using the generated labels as a
     # conditioner. We reshape the sampled labels to be
     # (batch_size, 1) so that we can feed them into the embedding
     # layer as a length one sequence
-    generated_images <- predict(generator, list(noise, sampled_labels))
+    generated_images <- predict(generator, noise)
     
     X <- abind(image_batch, generated_images, along = 1)
     y <- c(rep(1L, batch_size), rep(0L, batch_size)) %>% matrix(ncol = 1)
-    aux_y <- c(label_batch, sampled_labels) %>% matrix(ncol = 1)
-    
+
     # Check if the discriminator can figure itself out
     disc_loss <- train_on_batch(
       discriminator, x = X, 
-      y = list(y, aux_y)
+      y = y
     )
     
     epoch_disc_loss <- rbind(epoch_disc_loss, unlist(disc_loss))
@@ -223,11 +257,9 @@ for(epoch in 1:epochs){
     # Make new noise. Generate 2 * batch size here such that
     # the generator optimizes over an identical number of images as the
     # discriminator
-    noise <- runif(2*batch_size*latent_size, min = 0, max = 1) %>%
+    noise <- runif(2*batch_size*latent_size, min = -1, max = 1) %>%
       matrix(nrow = 2*batch_size, ncol = latent_size)
-    sampled_labels <- sample(0:1, size = 2*batch_size, replace = TRUE) %>%
-      matrix(ncol = 1)
-    
+
     # Want to train the generator to trick the discriminator
     # For the generator, we want all the {fake, not-fake} labels to say
     # not-fake
@@ -235,8 +267,8 @@ for(epoch in 1:epochs){
     
     combined_loss <- train_on_batch(
       combined, 
-      list(noise, sampled_labels),
-      list(trick, sampled_labels)
+      noise,
+      trick
     )
     
     epoch_gen_loss <- rbind(epoch_gen_loss, unlist(combined_loss))
@@ -248,21 +280,17 @@ for(epoch in 1:epochs){
   # Evaluate the testing loss here
   
   # Generate a new batch of noise
-  noise <- runif(num_test*latent_size, min = 0, max = 1) %>%
+  noise <- runif(num_test*latent_size, min = -1, max = 1) %>%
     matrix(nrow = num_test, ncol = latent_size)
   
-  # Sample some labels from p_c and generate images from them
-  sampled_labels <- sample(0:1, size = num_test, replace = TRUE) %>%
-    matrix(ncol = 1)
-  generated_images <- predict(generator, list(noise, sampled_labels))
+  generated_images <- predict(generator, noise)
   
   X <- abind(dataset$test$x, generated_images, along = 1)
   y <- c(rep(1, num_test), rep(0, num_test)) %>% matrix(ncol = 1)
-  aux_y <- c(dataset$test$y, sampled_labels) %>% matrix(ncol = 1)
-  
+
   # See if the discriminator can figure itself out...
   discriminator_test_loss <- evaluate(
-    discriminator, X, list(y, aux_y), 
+    discriminator, X, y, 
     verbose = FALSE
   ) %>% unlist()
   
@@ -271,14 +299,12 @@ for(epoch in 1:epochs){
   # Make new noise
   noise <- runif(2*num_test*latent_size, min = -1, max = 1) %>%
     matrix(nrow = 2*num_test, ncol = latent_size)
-  sampled_labels <- sample(0:9, size = 2*num_test, replace = TRUE) %>%
-    matrix(ncol = 1)
-  
+
   trick <- rep(1, 2*num_test) %>% matrix(ncol = 1)
   
   generator_test_loss = combined %>% evaluate(
-    list(noise, sampled_labels),
-    list(trick, sampled_labels),
+    noise, 
+    trick,
     verbose = FALSE
   )
   
@@ -286,59 +312,47 @@ for(epoch in 1:epochs){
   
   
   # Generate an epoch report on performance
-  row_fmt <- "\n%22s : loss %4.2f | %5.2f | %5.2f"
+  row_fmt <- "\n%22s : loss %4.2f "
   cat(sprintf(
     row_fmt, 
     "generator (train)",
-    generator_train_loss[1],
-    generator_train_loss[2],
-    generator_train_loss[3]
+    generator_train_loss[1]
   ))
   cat(sprintf(
     row_fmt, 
     "generator (test)",
-    generator_test_loss[1],
-    generator_test_loss[2],
-    generator_test_loss[3]
+    generator_test_loss[1]
   ))
   
   cat(sprintf(
     row_fmt, 
     "discriminator (train)",
-    discriminator_train_loss[1],
-    discriminator_train_loss[2],
-    discriminator_train_loss[3]
+    discriminator_train_loss[1]
   ))
   
   cat(sprintf(
     row_fmt, 
     "discriminator (test)",
-    discriminator_test_loss[1],
-    discriminator_test_loss[2],
-    discriminator_test_loss[3]
+    discriminator_test_loss[1]
   ))
   
   cat("\n")
   
   # Generate some digits to display
-  noise <- runif(2*latent_size, min = 0, max = 1) %>%
-    matrix(nrow = 2, ncol = latent_size)
-  
-  sampled_labels <- 0:1 %>%
-    matrix(ncol = 1)
+  noise <- runif(100*latent_size, min = -1, max = 1) %>%
+    matrix(nrow = 100, ncol = latent_size)
   
   # Get a batch to display
   generated_images <- predict(
     generator,    
-    list(noise, sampled_labels)
+    noise
   )
   
-  img <- NULL
-  for(i in 1:2){
-    img <- cbind(img, generated_images[i,,,])
-  }
+  plotImages(generated_images)
   
-  ((img + 1)/2) %>% as.raster() %>%
-    plot()
   
+
+
+  
+    
 }
--- graphics/src/scraper.R
+++ graphics/src/scraper.R
@@ -1,52 +1,108 @@
 library(tidyverse)
 library(jpeg)
 library(png)
-x <- c(
-  URLencode("ghost pixel art"),
-  URLencode("monster pixel art") 
-  
-)
-n <- 200
-# create url
-for(i in 1:length(x)){
-  cmd <- paste0("python scraper.py --search '",x[i],"' --num_images ",n," --directory '../data/'")
-  system(cmd)
-}
-imgs <- list.files("../data/")
-
+library("magick")
 #source("http://bioconductor.org/biocLite.R")
 #biocLite("EBImage")
 library("EBImage")
-for(i in 1:length(imgs)){
-  tryCatch({
-    x <- readImage(paste0("../data/",imgs[i]))
-    y <- resize(x, w = 64, h = 64)
-    image(y)
-    Sys.sleep(1)
-    EBImage::writeImage(y,paste0("../data/proc/",i,".png"))  
-  }, error = function(e){
-    print(e)
-  })
-}
 
 
-exportData <- function(path = "../data/proc/gm/"){
-  imgs <- list.files(path)
-  d = array(0, dim = c(length(imgs), 28,28))
+importData <- function(x = c(URLencode("ghost pixel art"), URLencode("monster pixel art")), n = 200){
+  # create url
+  for(i in 1:length(x)){
+    cmd <- paste0("python scraper.py --search '",x[i],"' --num_images ",n," --directory '../data/'")
+    system(cmd)
+  }
+  imgs <- list.files("../data/")
+  
+  
   for(i in 1:length(imgs)){
     tryCatch({
-      x <- readImage(paste0(path,imgs[i]))
-      y <- resize(x, w = 28, h = 28)
-      y <- y@.Data[,,1]+y@.Data[,,2]+y@.Data[,,3]
-      d[i,,] <- y / max(y)
-      image(d[i,,])
+      x <- readImage(paste0("../data/",imgs[i]))
+      y <- resize(x, w = 64, h = 64)
+      image(y)
       Sys.sleep(1)
+      EBImage::writeImage(y,paste0("../data/proc/",i,".png"))  
+    }, error = function(e){
+      print(e)
+    })
+  }  
+}
+
+exportData <- function(path = "../data/proc/gm/", export = TRUE){
+  imgs <- list.files(path)
+  d = array(0, dim = c(length(imgs)*4, 28,28))
+  dd = array(0, dim = c(length(imgs)*4, 28,28, 3))
+  index = 1
+  for(i in 1:length(imgs)){
+    tryCatch({
+      x <- image_read(paste0(path,imgs[i])) %>%
+        image_background(color = "#FFFFFF") %>%
+        as_EBImage()
+      img <- resize(x, w = 28, h = 28)
+      y <- img@.Data[,,1]+img@.Data[,,2]+img@.Data[,,3]
+      scaler <- max(y)/2
+      d[index,,] <- (y-scaler) / scaler
+      yy <- img@.Data[,,1:3]
+      di <- dim(yy)
+      yy <- apply(yy, 3, function(x)(x-max(x)/max(x)))
+      dim(yy) <- di
+      dd[index,,,] <- yy
+      index <- index + 1
+      img <- rotate(img, 0)
+      y <- img@.Data[,,1]+img@.Data[,,2]+img@.Data[,,3]
+      d[index,,] <- (y-scaler) / scaler
+      yy <- img@.Data[,,1:3]
+      di <- dim(yy)
+      yy <- apply(yy, 3, function(x)(x-max(x)/max(x)))
+      dim(yy) <- di
+      dd[index,,,] <- yy
+      index <- index + 1 
+      img <- rotate(img, 0)
+      y <- img@.Data[,,1]+img@.Data[,,2]+img@.Data[,,3]
+      print(dim(img@.Data))
+      d[index,,] <- (y-scaler) / scaler
+      yy <- img@.Data[,,1:3]
+      di <- dim(yy)
+      yy <- apply(yy, 3, function(x)(x-max(x)/max(x)))
+      dim(yy) <- di
+      dd[index,,,] <- yy
+      index <- index + 1 
+      img <- rotate(img, 0)
+      y <- img@.Data[,,1]+img@.Data[,,2]+img@.Data[,,3]
+      d[index,,] <- (y-scaler) / scaler
+      yy <- img@.Data[,,1:3]
+      di <- dim(yy)
+      yy <- apply(yy, 3, function(x)(x-max(x)/max(x)))
+      dim(yy) <- di
+      dd[index,,,] <- yy
+      index <- index + 1 
+      if(!export){
+        grid.raster((dd[index-1,,,] +1)/2)
+        Sys.sleep(1)
+      }
     }, error = function(e){
       print(e)
     })
   }
-  saveRDS(d, paste0(path,"data.dat"))
+  if(export){
+    saveRDS(d, paste0(path,"data.dat"))
+    saveRDS(dd, paste0(path,"cdata.dat"))  
+  }
+  
 }
+
+
+x <- c(
+  URLencode("ghost pixel art"),
+  URLencode("monster pixel art") 
+)
+n <- 200
+
+x <- URLencode("sprite block pixel art")
+importData(x,n)
 
 exportData()
 exportData(path = "../data/proc/pg/")
+
+exportData(path = "../data/proc/block/")
